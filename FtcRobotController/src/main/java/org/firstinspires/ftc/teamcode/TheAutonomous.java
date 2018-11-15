1dfa81525481f3648a82c5e3f2dae597b930ddb6
rj360 <aonedtop@gmail.com>
2018-11-14 20:46:45 -0800

    Added Comments to Autonomous Program

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/TheAutonomous.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/TheAutonomous.java
@@ -7,13 +7,20 @@ import com.qualcomm.robotcore.util.Range;
 
 @Autonomous
 public class TheAutonomous extends LinearOpMode{
+    //Create Color Sensor Variable
     ColorSensor colorSensor;
     public void runOpMode () {
+        //Initalize the Color Sensor
         colorSensor = hardwareMap.get(ColorSensor.class, "color");
+        //Wait until driver presses "Start"
         waitForStart();
+        //While OP Mofr is running
         while (opModeIsActive()) {
+            //Initalize the motor
             DcMotor awesome = hardwareMap.get(DcMotor.class,"atach");
+            //Set power of motor to 1
             awesome.setPower(1);
+            //Add color sensor data to display
             telemetry.addData("Color Sensor",colorSensor.alpha());
             telemetry.update();
         }
