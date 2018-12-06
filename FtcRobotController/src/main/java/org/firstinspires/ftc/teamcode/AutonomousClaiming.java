72b6353709eb22d58722c93498488985a613106b
rj360 <aonedtop@gmail.com>
2018-12-05 21:27:42 -0800

    DE PARKING IS NOW ADDED YEEEEEEEEEEEEEEE

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/AutonomousClaiming.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/AutonomousClaiming.java
@@ -10,17 +10,25 @@ public class AutonomousClaiming extends LinearOpMode{
     private DcMotor leftmotor;
     private DcMotor rightmotor;
     public void runOpMode() throws InterruptedException{
+        waitForStart();
         motorTest = hardwareMap.get(DcMotor.class, "motorTest");
         leftmotor = motorTest;
         motor2 = hardwareMap.get(DcMotor.class, "motor2");
         rightmotor = motor2;
-        leftmotor.setPower(-1);
-        rightmotor.setPower(1);
-        sleep(3000);
+        leftmotor.setPower(-0.5);
+        rightmotor.setPower(0.5);
+        sleep(4000);
         leftmotor.setPower(0);
         rightmotor.setPower(0);
         Servo servo0 = hardwareMap.get(Servo.class, "servo0");
         servo0.setPosition(180);
         sleep(1000);
+        leftmotor.setPower(0.5);
+        rightmotor.setPower(0.5);
+        sleep(400);
+        leftmotor.setPower(0.5);
+        rightmotor.setPower(-0.5);
+        sleep(7000);
+
     }
 }
